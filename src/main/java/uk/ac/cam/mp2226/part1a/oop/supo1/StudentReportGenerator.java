6504dc73cf7924ca0bc96bbe1ea05047ec33571f
Marcell Perger <marcell.perger.1@gmail.com>
2025-11-21 23:48:23 +0000

    feat: Finish question 1.4

--- src/main/java/uk/ac/cam/mp2226/part1a/oop/supo1/StudentReportGenerator.java
+++ src/main/java/uk/ac/cam/mp2226/part1a/oop/supo1/StudentReportGenerator.java
@@ -1,6 +1,12 @@
 package uk.ac.cam.mp2226.part1a.oop.supo1;
 
 public class StudentReportGenerator {
+    static final int DEFAULT_YEAR = 2024;
 
+    String reportTitle;
+
+    String generateReport() {
+        return reportTitle + "\n(...Report body...)";
+    }
 }
 
